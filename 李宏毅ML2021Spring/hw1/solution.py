3a938dc09c67d2d46e255eb521bf5197b899b068
Echo <73675472+hooooolyshit@users.noreply.github.com>
2022-04-06 16:25:21 +0800

    main

---
+++ 李宏毅ML2021Spring/hw1/solution.py
@@ -0,0 +1,12 @@
+
+# import packages
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
