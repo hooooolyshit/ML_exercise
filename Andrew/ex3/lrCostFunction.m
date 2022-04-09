2b9b4c54b24783ea477c17333a4473e3cc8b79b2
Echo <73675472+hooooolyshit@users.noreply.github.com>
2022-04-09 11:39:13 +0800

    solve Andrew ex4

--- Andrew/ex3/lrCostFunction.m
+++ Andrew/ex3/lrCostFunction.m
@@ -38,12 +38,12 @@ m = length(y); % number of training examples
 
 tempTheta = theta;
 tempTheta(1) = 0;
+h = sigmoid(X * theta);
+J = (-1 / m) * sum(y.*log(h) + (1 - y).*log(1 - h)) + (lambda / (2 * m))*sum(tempTheta.^2);
 
-J = (-1 / m) * sum(y.*log(sigmoid(X * theta)) + (1 - y).*log(1 - sigmoid(X * theta))) + (lambda / (2 * m))*sum(tempTheta.^2);
 temp = sigmoid (X * theta);
 error = temp - y;
-grad = (1 / m) * (X' * error) + (lambda/m)*tempTheta;
-
+grad = (1. / m) * (X' * error) + (lambda/m)*tempTheta;
 
 % =============================================================
 
--- Andrew/ex4/nnCostFunction1.m
+++ Andrew/ex4/nnCostFunction1.m
@@ -1,10 +1,11 @@
-function [J grad] = nnCostFunction(nn_params, ...
+function [J, grad] = nnCostFunction(nn_params, ...
                                    input_layer_size, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
                                    X, y, lambda)
 %NNCOSTFUNCTION Implements the neural network cost function for a two layer
 %neural network which performs classification
+
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
@@ -34,12 +35,26 @@ Theta2_grad = zeros(size(Theta2));
 % Instructions: You should complete the code by working through the
 %               following parts.
 %
-% Part 1: Feedforward the neural network and return the cost in the
+
+%% Part 1: Feedforward the neural network and return the cost in the
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
-% Part 2: Implement the backpropagation algorithm to compute the gradients
+% change into a logical array, the i_th number will be 1 with others 0;  
+h = eye(num_labels);
+y = h(y,:);
+
+a1 = [ones(m,1) X];
+z2 = sigmoidGradient(a1*Theta1');
+a2 = [ones(m,1) z2];
+a3 = sigmoid(a2 * Theta2');
+
+err = lambda/2/m * (sum(Theta1(:,2:end).^2, "all") + sum(Theta2(:,2:end).^2, "all"));
+
+J = (-1 / m) * sum(sum(y.*log(a3) + (1 - y).*log(1 - a3))) + err;
+
+%% Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
 %         Theta2_grad, respectively. After implementing Part 2, you can check
@@ -54,7 +69,26 @@ Theta2_grad = zeros(size(Theta2));
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
-% Part 3: Implement regularization with the cost function and gradients.
+delta3 = a3- y;
+delta2 = delta3*Theta2;
+delta2 = delta2(:,2:end) .* sigmoidGradient(z2);
+
+% gradiant J(theta_
+delta2 = delta2 .* a1;
+delta3 = delta3 .* a2;
+%%
+% 
+% $ D_{ij}^{(l)} :=\frac{1}{m}\Delta_{ij}^{(l)}+\lambda\Theta_{ij}^{(l)}$              ${if}\; j \neq  0$
+
+% $ D_{ij}^{(l)} :=\frac{1}{m}\Delta_{ij}^{(l)}$                             ${if}\; j = 0$ 
+
+% calc D
+Theta1_grad = delta2/m + lambda;
+Theta2_grad = delta3/m + ;
+
+
+
+%% Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
 %               backpropagation. That is, you can compute the gradients for
